e20c8787f17b5f2b0674073233f97e7edbdc023f
Tavahiura SANG <tavahiura.sang@cdbdx.biz>
2022-12-06 18:30:26 +0100

    day 4

--- 2022/4/main.ps1
+++ 2022/4/main.ps1
@@ -4,7 +4,7 @@ $data = Get-Content $InputFile
 
 $count = 1
 
-function BuildSection([string]$pair) {
+function BuildSection1([string]$pair) {
     $splitted = $pair -split '-'
     for ($i = [int]$splitted[0]; $i -le [int]$splitted[1]; $i++) {
         $section += "#$i#"
@@ -12,21 +12,44 @@ function BuildSection([string]$pair) {
     return [string]"$section"
 }
 
-function IsOverlapping([string]$first, [string]$second) {
+function IsOverlapping1([string]$first, [string]$second) {
     if ($first -match $second -or $second -match $first) {
         return $true
     }
     return $false
 }
 
-$count = 0
+function BuildSection2([string]$pair) {
+    $splitted = $pair -split '-'
+    $res = [System.Collections.Generic.List[int]]::new()
+    for ($i = [int]$splitted[0]; $i -le [int]$splitted[1]; $i++) {
+        $res.Add($i)
+    }
+    return $res
+}
+
+function IsOverlapping2([int[]]$first, [int[]]$second) {
+    foreach ($v in $first) {
+        if ($second -contains $v) {
+            return $true
+        }
+    }
+    return $false
+}
+
+$count1 = 0
+$count2 = 0
 foreach ($line in $data) {
     if ($line) {
         $elves = $line -split ","
-        if ($(IsOverlapping (BuildSection $elves[0]) (BuildSection $elves[1]))) {
-            $count++
+        if ($(IsOverlapping1 (BuildSection1 $elves[0]) (BuildSection1 $elves[1]))) {
+            $count1++
+        }
+        if ($(IsOverlapping2 (BuildSection2 $elves[0]) (BuildSection2 $elves[1]))) {
+            $count2++
         }
     }
 }
 
-Write-Output "TOTAL OVERLAP: $count"
+Write-Output "1) TOTAL OVERLAP: $count1"
+Write-Output "2) TOTAL OVERLAP: $count2"
