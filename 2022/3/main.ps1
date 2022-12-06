784227ab63dd6e0ed65c19d7de780f2c4a8a238c
Tavahiura SANG <tavahiura.sang@cdbdx.biz>
2022-12-06 19:21:18 +0100

    day 3

--- 2022/3/main.ps1
+++ 2022/3/main.ps1
@@ -2,8 +2,7 @@
 Param([string]$InputFile)
 $data = Get-Content $InputFile
 
-$result = @{}
-$count = 1
+$result = [System.Collections.Generic.List[System.Collections.Generic.List[string]]]::new()
 
 foreach ($line in $data) {
     if ($line) {
@@ -12,7 +11,7 @@ foreach ($line in $data) {
         $firstHalf = $line.Substring(0, $countHalf)
         $secondHalf = $line.Substring($countHalf, $countHalf)
 
-        $tmpList = [System.Collections.ArrayList]::new()
+        $tmpList = [System.Collections.Generic.List[string]]::new()
         for ($i = 0; $i -lt $countHalf; $i++) {
             $charTmp = $firstHalf.Substring($i, 1)
 
@@ -20,14 +19,13 @@ foreach ($line in $data) {
                 $tmpList.Add($charTmp) | Out-Null
             }
         }
-        $result.Add($count, @($tmpList | Get-Unique))
-        $count++
+        $result.Add(@($tmpList | Select-Object -Unique))
     }
 }
 
 $total = 0
-foreach ($h in $result.GetEnumerator() ) {
-    foreach ($val in $h.Value) {
+foreach ($h in $result) {
+    foreach ($val in $h) {
         if ($val -cmatch "^[a-z]*$") {
             $total += ([int]([char]$val) - 96)
         }
@@ -37,4 +35,43 @@ foreach ($h in $result.GetEnumerator() ) {
     }
 }
 
-Write-Output "TOTAL : $total"
+Write-Output "1) TOTAL : $total"
+
+$count = 1
+$GROUP_COUNT = 3
+
+$result = [System.Collections.Generic.List[System.Collections.Generic.List[string]]]::new()
+$tmpList = [System.Collections.Generic.List[string]]::new()
+
+foreach ($line in $data) {
+    if ($line) {
+        $tmpList.Add($line) | Out-Null
+        if (($count % $GROUP_COUNT) -eq 0) {
+            $result.Add($tmpList)
+            $tmpList = [System.Collections.Generic.List[string]]::new()
+        }
+        $count++
+    }
+}
+
+function FindBadge([System.Collections.Generic.List[string]]$groups) {
+    for ($j = 0; $j -lt $groups[0].Length; $j++) {
+        $charTmp = $groups[0].Substring($j, 1)
+        if ([regex]::match($groups[1], $charTmp).Success -and [regex]::match($groups[2], $charTmp).Success) {
+            return $charTmp
+        }
+    }
+}
+
+$total = 0
+foreach ($h in $result) {
+    $val = FindBadge $h
+    if ($val -cmatch "^[a-z]*$") {
+        $total += ([int]([char]$val) - 96)
+    }
+    else {
+        $total += ([int]([char]$val) - 38)
+    }
+}
+
+Write-Output "2) TOTAL : $total"
